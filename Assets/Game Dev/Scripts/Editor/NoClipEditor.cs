760f7c7131954bf0e83853f8b2d1d7ae0068668b
Jack Davenport <jack.davenport5011@gmail.com>
2020-03-02 13:00:12 +1100

    Created noclip camera script

---
+++ Assets/Game Dev/Scripts/Editor/NoClipEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NoClip))]
+public class NoClipEditor : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        NoClip script = target as NoClip;
+
+        // introduction at beginning of script
+        EditorGUILayout.LabelField("No Clip Camera", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("A free-moving camera which does not collide with objects");
+        EditorGUILayout.Space();
+
+        // debug option (if in play mode)
+        if(Application.isPlaying) {
+            Vector2 rotation = script.getRotation();
+            EditorGUILayout.LabelField("Current Rotation", EditorStyles.boldLabel);
+            AddKeyValuePair("X", rotation.x.ToString());
+            AddKeyValuePair("Y", rotation.y.ToString());
+            EditorGUILayout.Space();
+        }
+
+        // controls
+        EditorGUILayout.LabelField("Controls", EditorStyles.boldLabel);
+        AddKeyValuePair("WASD/Arrow Keys", "Move");
+        AddKeyValuePair("Mouse", "Look Around");
+        AddKeyValuePair("Shift", "Move faster");
+        AddKeyValuePair("Q/E", "Move up and down");
+        EditorGUILayout.Space();
+
+        // movement
+        EditorGUILayout.LabelField("Movement Settings", EditorStyles.boldLabel);
+        script.moveSpeed = EditorGUILayout.FloatField("Move Speed", script.moveSpeed);
+        script.shiftMultiplier = EditorGUILayout.FloatField("Shift Multiplier", script.shiftMultiplier);
+        script.smoothSpeed = EditorGUILayout.FloatField("Smooth Speed", script.smoothSpeed);
+        EditorGUILayout.Space();
+
+        // 
+        EditorGUILayout.LabelField("Mouse Settings", EditorStyles.boldLabel);
+        script.lookSpeed = EditorGUILayout.Slider("Look Speed", script.lookSpeed, 0f, 360f);
+        script.lookLimit = EditorGUILayout.Slider("Up/Down Limit", script.lookLimit, 0f, 360f);
+        script.invertY = EditorGUILayout.Toggle("Invert Y-axis?", script.invertY);
+        EditorGUILayout.Space();
+    }
+
+    private void AddKeyValuePair(string key, string value)
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            EditorGUILayout.LabelField(key, EditorStyles.miniBoldLabel);
+            EditorGUILayout.LabelField(value);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+}
---
+++ Assets/Game Dev/Scripts/NoClip.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class NoClip : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float shiftMultiplier = 2;
+    public float smoothSpeed = 3;
+    public float lookSpeed = 180;
+    public bool invertY = false;
+    public float lookLimit = 90f;
+
+    private float move = 0;
+    private float strafe = 0;
+    private bool shift = false;
+    private float tempMove = 0;
+    private float tempStrafe = 0;
+    private float moveVel, strafeVel;
+    private float rotX, rotY;
+
+    void Start() {
+        Vector3 rot = transform.rotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+    }
+
+    // actually move the camera each physics update
+    void FixedUpdate()
+    {
+        float smooth = Time.fixedDeltaTime * this.smoothSpeed;
+        this.tempMove = Mathf.SmoothDamp(this.tempMove, this.move, ref this.moveVel, smooth);
+        this.tempStrafe = Mathf.SmoothDamp(this.tempStrafe, this.strafe, ref this.strafeVel, smooth);
+        // calculate move speed and add it to the camera's position
+        float speed = this.moveSpeed * (this.shift ? this.shiftMultiplier : 1f) * Time.fixedDeltaTime;
+        transform.position += ((transform.forward * this.tempMove) + (transform.right * this.tempStrafe)) * speed;
+    }
+
+    // get input from keyboard
+    void Update()
+    {
+        // update movement variables
+        this.move = Input.GetAxis("Vertical");
+        this.strafe = Input.GetAxis("Horizontal");
+        this.shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        // rotate the camera based on mouse movement
+        float speed = this.lookSpeed * Time.deltaTime;
+        rotX = rotX + (Input.GetAxis("Mouse Y") * speed * (this.invertY ? 1f : -1f));
+        rotY = (rotY + (Input.GetAxis("Mouse X") * speed)) % 360f;
+        rotX = Mathf.Clamp(rotX, -this.lookLimit+0.1f, this.lookLimit-0.1f); // x axis needs to be limited so you can't look backwards
+        transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+    }
+
+    public Vector2 getRotation()
+    {
+        return new Vector2(rotX, rotY);
+    }
+
+}
