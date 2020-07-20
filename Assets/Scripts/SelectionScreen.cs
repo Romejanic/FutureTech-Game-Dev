277c39eca40db5bdbc25d21f69c12f7771c96501
Jack Davenport <jack.davenport5011@gmail.com>
2020-07-20 16:01:13 +1000

    Added selection screen, deleted unnecessary scenes

---
+++ Assets/Scripts/SelectionScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionScreen : MonoBehaviour
+{
+
+    public void OpenScene(int buildIdx)
+    {
+        Debug.Log("Open Scene (" + buildIdx + ")");
+        SceneManager.LoadScene(buildIdx);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+}
