363ee882ca2bb4d28432e2b5a7ef116fe43d4759
Jack Davenport <jack.davenport5011@gmail.com>
2020-07-25 00:44:25 +1000

    Created basic flying AI for robots

---
+++ Assets/Game Dev/Scripts/RobotFlying.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class RobotFlying : MonoBehaviour
+{
+    public float searchRange = 20f;
+    public float hoverRange = 5f;
+    public float turnSpeed = 10f;
+    public float flySpeed = 5f;
+
+    private Transform player;
+    private Rigidbody rb;
+    private bool spottedPlayer = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // find the player in the scene
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if(!playerObj) {
+            Debug.Log("Found no player to kill! Make sure there's an object with the tag \"Player\"");
+        } else {
+            player = playerObj.transform;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!player)
+            return;
+        float sqrDist = Vector3.SqrMagnitude(transform.position - player.position);
+        if (!spottedPlayer) {
+            if(sqrDist < searchRange * searchRange) {
+                spottedPlayer = true;
+            }
+        } else {
+            float sqrHover = hoverRange * hoverRange;
+            Vector3 toPlayer = (player.position - transform.position).normalized;
+            Quaternion rot = Quaternion.LookRotation(toPlayer);
+            rb.MoveRotation(Quaternion.Slerp(transform.rotation, rot, this.turnSpeed * Time.fixedDeltaTime));
+            Vector3 toPlayerScaled = toPlayer * this.flySpeed * Time.fixedDeltaTime;
+            if (sqrDist > sqrHover) {
+                rb.MovePosition(transform.position + toPlayerScaled);
+            } else if(sqrDist < sqrHover) {
+                rb.MovePosition(transform.position - toPlayerScaled);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, hoverRange);
+    }
+
+}
--- README.md
+++ README.md
@@ -3,8 +3,14 @@ The instructor project for the FutureTech Game Dev workshop.
 
 **Current Project Version:** Unity 2019.3.1f1
 
+## Downloads
+- **Instructor Examples (Windows)**
+- **Instructor Examples (Mac)**
+- **FutureTech Game Dev package**
+
 ## Credits
 - Unity Standard Assets
 - [textures.com](https://textures.com/)
 - [Rock model](https://free3d.com/3d-model/low-poly-rock-4631.html) by [tyrosmith](https://free3d.com/user/tyrosmith)
 - [Sting Sword model](https://free3d.com/3d-model/sting-sword-128810.html) by [kangarooz3d](https://free3d.com/user/kangarooz3d)
+- [Enemy Robots](https://assetstore.unity.com/packages/3d/characters/enemy-robots-134968) by [WatchFindDoMedia](https://assetstore.unity.com/publishers/8326)
